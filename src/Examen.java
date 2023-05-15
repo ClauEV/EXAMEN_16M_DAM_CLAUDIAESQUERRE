1e212fbbcd86c69e5bce728a207c389acb98d0ae
ALUMNOS <ALUMNOS@P619>
2023-05-15 09:12:40 +0200

    nuevo commit

---
+++ src/Examen.java
@@ -0,0 +1,11 @@
+
+public class Examen {
+	
+	public static void main(String[] arg) {
+		
+		System.out.println("Hola Ceinmark, soy Claudia Esquerre");
+		
+		System.out.println("Introduce un numero para averiguar si es par o no");
+	}
+
+}
